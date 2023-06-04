671a209b96335781d302f678dd3fe0a1e81a3749
LouisF2000 <louis-henry.foersch@et.hs-fulda.de>
2023-06-04 23:12:51 +0200

    Startpage optimiert

--- Working_time_management/Startpage.xaml.cs
+++ Working_time_management/Startpage.xaml.cs
@@ -25,20 +25,38 @@ namespace Working_time_management
             InitializeComponent();
         }
 
+        private bool isLogIn = false;
+
         private void LogInEvent(object sender, RoutedEventArgs e)
-        {
-            LogInFrame.Content = new Log_in();
-            btnLogIn.Visibility = Visibility.Visible;
+        {   
+            isLogIn = true;
+            lblAnm.Visibility = Visibility.Visible;
+            lblPwd.Visibility = Visibility.Visible;
+            tbPwd.Visibility = Visibility.Visible;
+            Grid.SetRow(btnLogIn, 6);
+            Anmeldung.FontSize = 18;
+            Anmeldung.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#FFAFAFAF");
+            btnTimeDtc.FontSize = 15;
+            btnTimeDtc.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#FFDDDDDD");
         }
 
         private void btnLogInSuc(object sender, RoutedEventArgs e)
         {
+            //if(isLogIn)
             this.NavigationService.Navigate(new menu());
         }
 
         private void TimeDtc(object sender, RoutedEventArgs e)
         {
-            //this.NavigationService.Navigate(new ());
+            isLogIn = false;
+            lblAnm.Visibility = Visibility.Hidden;
+            lblPwd.Visibility = Visibility.Hidden;
+            tbPwd.Visibility = Visibility.Hidden;
+            Grid.SetRow(btnLogIn, 4);
+            Anmeldung.FontSize = 15;
+            Anmeldung.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#FFDDDDDD");
+            btnTimeDtc.FontSize = 18;
+            btnTimeDtc.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#FFAFAFAF");
         }
     }
 }
