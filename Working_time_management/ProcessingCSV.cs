aeca51b842485f778bc345798ef893f06ed643a7
LouisF2000 <louis-henry.foersch@et.hs-fulda.de>
2023-07-13 19:51:04 +0200

    Kommentare ProcessingCSV fertig

--- Working_time_management/ProcessingCSV.cs
+++ Working_time_management/ProcessingCSV.cs
@@ -162,7 +162,7 @@ namespace Working_time_management
             return workerInformation; 
         }
 
-        public static string[] getAllWorkerRequests(string id)          //Gibt alle <anträge dieses Mitarbiters zurück
+        public static string[] getAllWorkerRequests(string id)          //Gibt alle Anträge dieses Mitarbiters zurück
         {
             return File.ReadAllLines(getUserRequestPath(id));
         }
@@ -191,7 +191,7 @@ namespace Working_time_management
             File.WriteAllLines(getWorkingTimeInformationPath(id), information, Encoding.UTF8);          //CSV zur Eintragung der Überstunden, der Urlaubstage und, ob eine Pause genommen wurde, wird erstellt
 
         }
-        public static void writeComeInCSV(string id, DateTime checkIn)
+        public static void writeComeInCSV(string id, DateTime checkIn)      // Verarbeitet einstempeln des Mitarbeiters
         {
             if (MainWindow.timeRounding == 15)                  // Aufrundung der Arbeitszeit, je nachdem, ob die Rundung nach 1, 5 oder 15 Minuten eingestellt ist
             {
@@ -205,43 +205,40 @@ namespace Working_time_management
             {
                 checkIn = roundUp(checkIn, TimeSpan.FromMinutes(1));
             }
-            string checkInString = ";" + checkIn.ToString("HH:mm");
-            string currentDate = DateTime.Now.ToString("dd.MM.yyyy");
+            string checkInString = ";" + checkIn.ToString("HH:mm");     // Eingangsuhrzeit formartieren als Uhrzeit und für CSV
+            string currentDate = checkIn.ToString("dd.MM.yyyy");   // Eingangsdatum speichern
             bool dateFound = false;
-            foreach (string line in File.ReadLines(getUserPathWorkingTimeCSV(id)))
+            foreach (string line in File.ReadLines(getUserPathWorkingTimeCSV(id)))      // Suchen, ob aktuelles Datum schon in CSV existiert
             {
                 string[] data = line.Split(';');
                 string date = data[0];
                 if (currentDate == date)
                 {
-                    dateFound = true;
+                    dateFound = true;       // Wenn Datum vorhanden: Flag setzen
                     break;
                 }
             }
-            if(!dateFound)
+            if(!dateFound)                  // Wenn Datum nicht vorhanden: Neue Zeile für aktuellen Tag in CSV anlegen
             {
-                //neue Zeille für Datum anlegen
                 checkInString= "\r\n" +  currentDate + checkInString;
                 string[] timeInformation = File.ReadAllLines(getWorkingTimeInformationPath(id))[1].Split(';');
-                //Pause zurücksetzen und Überstunden vorbereiten
-                timeInformation[0] = "n";
+                timeInformation[0] = "n";       // Neuer Tag --> Pause zurücksetzen
                 string[] overtime = timeInformation[1].Split(':');
                 int overTimeHoursCome = 0;
-                if (overtime[0].Contains('.'))              //Wenn "overtime" die Zeit in Tagen und nicht mehr in Stunden gespeichert hat, wird diese hier wieder in Stunden umgewandelt
+                if (overtime[0].Contains('.'))              // Wenn "overtime" die Zeit als TimeSpan mit Tagen und nicht mehr in Stunden gespeichert hat, wird diese hier wieder in Stunden umgewandelt
                 {
-                    overTimeHoursCome = int.Parse(overtime[0].Split('.')[0]) * 24;
+                    overTimeHoursCome = int.Parse(overtime[0].Split('.')[0]) * 24 + int.Parse(overtime[0].Split('.')[1]);
                 }
                 else
                 {
                     overTimeHoursCome = int.Parse(overtime[0]);
                 }
-                string[] newovertime = new TimeSpan(overTimeHoursCome - 8, int.Parse(overtime[1]), 0).ToString().Split(':');
-                timeInformation[1] = newovertime[0] + ":" + newovertime[1];
+                timeInformation[1] = overTimeHoursCome - 8 + ":" + int.Parse(overtime[1]);     // Neuer Tag --> Einmnalig 8 Std abziehen, die im Laufe des Tages wieder gearbeitet werden müssen
                 File.WriteAllText(getWorkingTimeInformationPath(id), "Pause;Überstunden;Resturlaub\n" + timeInformation[0] + ";" + timeInformation[1] + ";" + timeInformation[2], Encoding.UTF8);
             }
-            File.AppendAllText(getUserPathWorkingTimeCSV(id), checkInString, Encoding.UTF8);
+            File.AppendAllText(getUserPathWorkingTimeCSV(id), checkInString, Encoding.UTF8); // Wenn Tag schon vorhanden: nur CheckIn Uhrzeit in CSV anhängen, ohne neue Zeile
         }
-        public static void writeGoInCSV(string id, DateTime checkOut)
+        public static void writeGoInCSV(string id, DateTime checkOut)   // Verarbeitet einstempeln des Mitarbeiters
         {
             if (MainWindow.timeRounding == 15)                      //Abrundung der Arbeitszeit, je nachedem, ob 1, 5 oder 15 Minuten eingestellt wurden
             {
@@ -264,55 +261,64 @@ namespace Working_time_management
             int totalHours = 0;
             int totalMinutes = 0;
             string[] totalWorkingTimeString = { "00", "00" };
-            foreach (string line in File.ReadLines(getUserPathWorkingTimeCSV(id)))
+            foreach (string line in File.ReadLines(getUserPathWorkingTimeCSV(id)))      // Auslesen der Arbeitszeiten
             {
                 string[] data = line.Split(';');
                 string date = data[0];
-                if (currentDate == date)
+                if (currentDate == date)                                                // Datum abgleichen          
                 {
-                    dateFound = true;
-                    string lastCheckIn = data[data.Length - 1];
+                    dateFound = true;                                                   // Flag setzen
+                    string lastCheckIn = data[data.Length - 1];                         // Letzten Eintrag einlesen
                     if (data.Length >= 5)
                     {
                         string[] currentWorkingTimeString = data[data.Length - 2].Split(":");
                         
-                        totalHours = int.Parse(currentWorkingTimeString[0]);
+                        totalHours = int.Parse(currentWorkingTimeString[0]);            // Tagesgesamtzeit auslesen
                         totalMinutes = int.Parse(currentWorkingTimeString[1]);
-                        if (currentWorkingTimeString[0].StartsWith('-'))
+                        if (currentWorkingTimeString[0].StartsWith('-'))                // TimeSpan negativ? --> Minuten negieren für TimeSpan Konstruktor
                         {
                             totalMinutes *= -1;
                         }
                     }
-                    string[] lastCheckInTime = lastCheckIn.Split(':');
-                    DateTime lastCheckInDateTime = new DateTime(today.Year, today.Month, today.Day, int.Parse(lastCheckInTime[0]), int.Parse(lastCheckInTime[1]), 0);
-                    TimeSpan workingTime = checkOut.Subtract(lastCheckInDateTime);
-                    if (workingTime < new TimeSpan(0, 0, 0))
+                    string[] lastCheckInTime = lastCheckIn.Split(':');              
+                    DateTime lastCheckInDateTime = new DateTime(today.Year, today.Month, today.Day, int.Parse(lastCheckInTime[0]), int.Parse(lastCheckInTime[1]), 0);   // Letztes Einstempeln als DateTime speichern
+                    TimeSpan workingTime = checkOut.Subtract(lastCheckInDateTime);      // Arbeitszeit berechnen
+                    if (workingTime < new TimeSpan(0, 0, 0))                            
                     {
-                        workingTime = new TimeSpan(0, 0, 0);
+                        workingTime = new TimeSpan(0, 0, 0);                            // negtiver Zeitabschnitt nullen (komnmt durch die Rundung)
                     }
-                    TimeSpan totalWorkingTime = new TimeSpan(totalHours, totalMinutes, 0).Add(workingTime);
-                    string[] timeInformation = File.ReadAllLines(getWorkingTimeInformationPath(id))[1].Split(';');
-                    string[] overtime = timeInformation[1].Split(':');
+                    TimeSpan totalWorkingTime = new TimeSpan(totalHours, totalMinutes, 0).Add(workingTime);         // Tagesgesamtzeit  mit Zeitabschnitt addiern
+                    string[] timeInformation = File.ReadAllLines(getWorkingTimeInformationPath(id))[1].Split(';');  // Zeitinfrmationen auslesen
+                    string[] overtime = timeInformation[1].Split(':');                                              // Überstunden auslesen
                     int overTimeHours = 0;
+                    int overTimeMinutes = int.Parse(overtime[1]);
                     if (overtime[0].Contains('.')) //Wenn "overtime" die Zeit in Tagen und nicht mehr in Stunden gespeichert hat, wird diese hier wieder in Stunden umgewandelt
                     {
                         overTimeHours = int.Parse(overtime[0].Split('.')[0]) * 24;
+                        if (overtime[0].StartsWith('-'))
+                        {
+                            overTimeHours -= int.Parse(overtime[0].Split('.')[0]);
+                            overTimeMinutes *= -1;      // Überstunden negativ? --> Minuten negieren für TimeSpan Konstruktor
+                        }
+                        else
+                        {
+                            overTimeHours += int.Parse(overtime[0].Split('.')[0]);
+                        }
                     }
                     else
                     {
-                        overTimeHours = int.Parse(overtime[0]);
+                        overTimeHours = int.Parse(overtime[0]); 
+                        if (overtime[0].StartsWith('-'))
+                        {
+                            overTimeMinutes *= -1;      // Überstunden negativ? --> Minuten negieren für TimeSpan Konstruktor
+                        }
                     }
-                    int overTimeMinutes = int.Parse(overtime[1]);
-                    if (overtime[0].StartsWith('-'))
+                    TimeSpan overtimeSpan = new TimeSpan(overTimeHours, overTimeMinutes, 0).Add(workingTime);   // Zeitabschnitt auf Überstunden-Konto addieren
+                    if (timeInformation[0] == "n" && MainWindow.breakAfterHours != 0 && totalWorkingTime.Hours >= MainWindow.breakAfterHours) // nach fixer Stundenzahl 45 Min abziehen
                     {
-                        overTimeMinutes *= -1;
-                    }
-                    TimeSpan overtimeSpan = new TimeSpan(overTimeHours, overTimeMinutes, 0).Add(workingTime);
-                    if (timeInformation[0] == "n" && MainWindow.breakAfterHours != 0 && totalWorkingTime.Hours >= MainWindow.breakAfterHours) // nach fixer Stundenzahl halbe Std abziehen
-                    {
-                        totalWorkingTime = totalWorkingTime.Subtract(new TimeSpan(0, 45, 0)); // nach fixer Uhrzeit 45 Minuten abziehen
-                        overtimeSpan = overtimeSpan.Subtract(new TimeSpan(0, 45, 0)); // nach fixer Uhrzeit 45 Minuten abziehen
-                        timeInformation[0] = "j";
+                        totalWorkingTime = totalWorkingTime.Subtract(new TimeSpan(0, 45, 0)); // Gesamtzeit 45 Minuten abziehen
+                        overtimeSpan = overtimeSpan.Subtract(new TimeSpan(0, 45, 0)); // Überstunden 45 Minuten abziehen
+                        timeInformation[0] = "j";                                     // Pause für den Tag vermerken
                     }
                     else if (timeInformation[0] == "n" && MainWindow.fixBreakTime != null)
                     {
@@ -320,48 +326,49 @@ namespace Working_time_management
                         int breakHours = int.Parse(breakTimeString[0]);
                         int breakMinutes = int.Parse(breakTimeString[1]);
                         DateTime breakTime = new DateTime(today.Year, today.Month, today.Day, breakHours, breakMinutes, 0);
-                        if (lastCheckInDateTime < breakTime && checkOut >= breakTime)
+                        if (lastCheckInDateTime < breakTime && checkOut >= breakTime)   // Prüfung, ob Anmeldezeitraum über Pausenzeit ging
                         {
-                            totalWorkingTime = totalWorkingTime.Subtract(new TimeSpan(0, 45, 0)); // nach fixer Uhrzeit 45 Minuten abziehen
-                            overtimeSpan = overtimeSpan.Subtract(new TimeSpan(0, 45, 0)); // nach fixer Uhrzeit 45 Minuten abziehen
-                            timeInformation[0] = "j";
+                            totalWorkingTime = totalWorkingTime.Subtract(new TimeSpan(0, 45, 0)); // Gesamtzeit 45 Minuten abziehen
+                            overtimeSpan = overtimeSpan.Subtract(new TimeSpan(0, 45, 0)); // Überstunden 45 Minuten abziehen
+                            timeInformation[0] = "j";   // Pause für den Tag vermerken
                         }
                     }
+                    // nochmal Überstunden auf Negatives Vorzeichen und größer 1 Tag prüfen
                     string[] newovertime = overtimeSpan.ToString().Split(':');
                     if (newovertime[0].Contains('.'))
                     {
                         int fullHours = int.Parse(newovertime[0].Split('.')[0]) * 24;
                         if (newovertime[0].StartsWith('-'))
                         {
-                            fullHours -= int.Parse(newovertime[0].Split('.')[1]);
+                            fullHours -= int.Parse(overtime[0].Split('.')[1]);
                         }
                         else
                         {
-                            fullHours += int.Parse(newovertime[0].Split('.')[1]);
+                            fullHours += int.Parse(overtime[0].Split('.')[1]);
                         }
                         newovertime[0] = fullHours.ToString();
                     }
-                    timeInformation[1] = newovertime[0] + ":" + newovertime[1];
-                    File.WriteAllText(getWorkingTimeInformationPath(id), "Pause;Überstunden;Resturlaub\n" + timeInformation[0] + ";" + timeInformation[1] + ";" + timeInformation[2], Encoding.UTF8);
+                    timeInformation[1] = newovertime[0] + ":" + newovertime[1];  // Überstunden zu String
+                    File.WriteAllText(getWorkingTimeInformationPath(id), "Pause;Überstunden;Resturlaub\n" + timeInformation[0] + ";" + timeInformation[1] + ";" + timeInformation[2], Encoding.UTF8);   // Überstunden abspeichern
                     totalWorkingTimeString = totalWorkingTime.ToString().Split(':');
                 }
                 lastDate=date;
             }
-            if (dateFound)
+            if (dateFound)      // Flag gesetzt? 
             {
-                string data = ";" + checkOutString + ";" + totalWorkingTimeString[0] + ":" + totalWorkingTimeString[1];
-                File.AppendAllText(getUserPathWorkingTimeCSV(id), data, Encoding.UTF8);
+                string data = ";" + checkOutString + ";" + totalWorkingTimeString[0] + ":" + totalWorkingTimeString[1];     // Daten für CSV formatieren
+                File.AppendAllText(getUserPathWorkingTimeCSV(id), data, Encoding.UTF8);                                     // Daten an CSV anhängen, ohne neue Zeile
             }
-            else //Error-Handling
+            else //Error-Handling und Tageswechsel
             {
-                if(lastDate != currentDate && lastDate != "Datum" && lastDate != "")
+                if(lastDate != currentDate && lastDate != "Datum" && lastDate != "")        // Prüfen, ob das letzte Einstempeln sinnvoll eingelesen wurde
                 {
-                    string[] lastDateDMY = lastDate.Split(".");
-                    DateTime lastDateDateTime = new DateTime(int.Parse(lastDateDMY[2]), int.Parse(lastDateDMY[1]), int.Parse(lastDateDMY[0]), 23, 59, 00);
-                    writeGoInCSV(id,lastDateDateTime);                   
+                    string[] lastDateDMY = lastDate.Split(".");                             // letztes Anmeldedatum auslesen
+                    DateTime lastDateDateTime = new DateTime(int.Parse(lastDateDMY[2]), int.Parse(lastDateDMY[1]), int.Parse(lastDateDMY[0]), 23, 59, 00);  // DateTime erstellen von diesem mTag um 23:59
+                    writeGoInCSV(id,lastDateDateTime);                                      // Checkout zu diesem Zeitpunkt --> Tageswechsel
                 }                                                               
-                writeComeInCSV(id, new DateTime(today.Year, today.Month, today.Day, 0, 0, 0));
-                writeGoInCSV(id, checkOut);
+                writeComeInCSV(id, new DateTime(today.Year, today.Month, today.Day, 0, 0, 0));  //CheckIn am aktuellen Tag um 00:00 nachholen
+                writeGoInCSV(id, checkOut);                                                     //CheckOut nochmal durchführen 
             }
 
         }
