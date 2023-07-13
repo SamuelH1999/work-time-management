f4758b6cd23e1b434816b3d2f3b2a4462ed23331
LouisF2000 <louis-henry.foersch@et.hs-fulda.de>
2023-07-13 18:17:06 +0200

    Kommentare request

--- Working_time_management/request.xaml.cs
+++ Working_time_management/request.xaml.cs
@@ -28,71 +28,71 @@ namespace Working_time_management
             InitializeComponent();
             this.userID = id;
             this.CSVpath = ProcessingCSV.getUserRequestPath(userID);
-            foreach (string line in File.ReadLines(CSVpath))
+            foreach (string line in File.ReadLines(CSVpath))        // Alle existiernden Anträge des Users auslesen
             {
-                string[] data = line.Split(';');
-                string header = data[0];
-                if (data[0] != "Abwesenheitsgrund")
+                string[] data = line.Split(';');                    // CSV-Spalten splitten
+                if (data[0] != "Abwesenheitsgrund")                 // Überschrift-Zeile ignorieren
                 {
                     string[] requestInformation = line.Split(";");
                     ListBoxItem newRequest = new ListBoxItem();
                     newRequest.Content = requestInformation[0] + ", " + requestInformation[1] + " - " + requestInformation[2] + ", " + requestInformation[3];
-                    allRequest.Items.Add(newRequest);
+                    allRequest.Items.Add(newRequest);               // Alle Anträge auf der Oberfläche anzeigen
+
                 }
             }
         }
 
-        private void clickNewRequest(object sender, RoutedEventArgs e)
+        private void clickNewRequest(object sender, RoutedEventArgs e)      // Seite für neuen Antrag laden
         {
             this.NavigationService.Navigate(new newRequest(userID));
         }
 
-        private void clickDeleteRequest(object sender, RoutedEventArgs e)
+        private void clickDeleteRequest(object sender, RoutedEventArgs e)   // Löscht ausgewählte Anträge
         {
-            ListBoxItem deleteItem = allRequest.SelectedItem as ListBoxItem;
+            ListBoxItem deleteItem = allRequest.SelectedItem as ListBoxItem;    // Checkt, ob ein Element in der Liste ausgewählt wurde
             if(deleteItem != null )
             {
-                string[] details = deleteItem.Content.ToString().Split(", ");
+                string[] details = deleteItem.Content.ToString().Split(", ");   // Content des Eintrags in Bestandteile splitten und Informationen speichern
                 string reason = details[0];
                 string[] timeSpan = details[1].Split(" - ");
                 string from = timeSpan[0];
                 string until = timeSpan[1];
                 string status = details[2];
-                if(status != "genehmigt")
+                if(status != "genehmigt")                                       // Nur offene oder abgelehnte Anträge können aus der Übersicht entfernt werden
                 {
                     bool wasFound = false;
-                    string[] requestCSV = File.ReadAllLines(CSVpath);
-                    string[] newCSVstring = new string[requestCSV.Length-1];
+                    string[] requestCSV = File.ReadAllLines(CSVpath);           // Alle Anträge einlesen
+                    string[] newCSVstring = new string[requestCSV.Length-1];    // Array für neue Liste ohne den gelöschten Antrag (1 Entrag weniger --> Length - 1)
                     for( int i = 0; i < requestCSV.Length; i++)
                     {
                         string[] requestInformation = requestCSV[i].Split(";");
-                        if (!wasFound && requestInformation[0] == reason && requestInformation[1] == from && requestInformation[2] == until && requestInformation[3] == status)
+                        if (!wasFound && requestInformation[0] == reason && requestInformation[1] == from && requestInformation[2] == until && requestInformation[3] == status)     // Informationen der CSV mit den gespeicherten Informationen abgleichen
                         {
-                            wasFound = true;
+                            wasFound = true;                                    // Flag wenn Eintrag gefunden und kein Eintrag in newString[i]
                         }
                         else
                         {
-                            if(!wasFound) {
-                                if(i < newCSVstring.Length)
+                            if(!wasFound) {                                     // Solange Eintrag nicht gefunden: Zeilen kopieren
+                                if(i < newCSVstring.Length)                     // Fehler vermeiden falls bis zur letzten Zeile kein Eintrag gefunden wird
                                 {
                                     newCSVstring[i] = requestCSV[i];
                                 }
                             }
                             else 
                             {
-                                newCSVstring[i-1] = requestCSV[i];
+                                newCSVstring[i-1] = requestCSV[i];              // Nachdem Eintrag gefunden: Kopie nun verschoben, wegen gelöschter Zeile
                             }
                         }
                     }
-                    if (wasFound)
+                    if (wasFound)                                               // Wenn Durchlauf erfolgreich:
                     {
-                        allRequest.Items.Remove(deleteItem);
-                        File.WriteAllLines(CSVpath, newCSVstring);
+                        allRequest.Items.Remove(deleteItem);                    // Eintrag aus UI-Liste löschen
+                        File.WriteAllLines(CSVpath, newCSVstring);              // CSV mit Kopie überschreiben
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Antrag bereits bewilligt!", "Fehler", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK);
+                    MessageBox.Show("Antrag bereits bewilligt!", "Fehler", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK);      // Fehler, falls Antrag bewilligt
                 }
             }
             
